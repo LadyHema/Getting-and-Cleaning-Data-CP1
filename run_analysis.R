ef4436030faa3dc25836d779fca995192392fd33
LadyHema <njuggel@gmail.com>
2018-02-22 23:23:13 +0200

    change file format

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,12 @@ library(ggplot2)
 library(dplyr)
 library(data.table)
 library(WriteXLS)
-download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-              destfile = "run.zip", method = "curl")
-unzip("run.zip")
-setwd("UCI HAR Dataset")
-
+# setwd("~/Documents/R/coursera/c3w4")
+# download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+#               destfile = "run.zip", method = "curl")
+# unzip("run.zip")
+# setwd("UCI HAR Dataset")
+# getwd()
 
 #download and unpack this script in folder with downloaded and unpacked "UCI HAR Dataset"
 #run the script
